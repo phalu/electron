c8882c7b3a820b6a55790a6f153534a82cf94f06
phalu <mailphalu@gmail.com>
2020-11-09 14:10:58 -0500

    second commit

---
+++ lessons/3. Main Process API/8. Tray/main.js
@@ -0,0 +1,63 @@
+// Modules
+const {app, BrowserWindow, Tray, Menu} = require('electron')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow, tray
+
+let trayMenu = Menu.buildFromTemplate([
+  { label: 'Item 1' },
+  { role: 'quit' }
+])
+
+function createTray () {
+
+  tray = new Tray('trayTemplate@2x.png')
+  tray.setToolTip('Tray details')
+
+  tray.on('click', e => {
+
+    if (e.shiftKey) {
+      app.quit()
+    } else {
+      mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show()
+    }
+  })
+
+  tray.setContextMenu(trayMenu)
+}
+
+// Create a new BrowserWindow when `app` is ready
+function createWindow () {
+
+  createTray()
+
+  mainWindow = new BrowserWindow({
+    width: 1000, height: 800,
+    webPreferences: { nodeIntegration: true }
+  })
+
+  // Load index.html into the new BrowserWindow
+  mainWindow.loadFile('index.html')
+
+  // Open DevTools - Remove for PRODUCTION!
+  mainWindow.webContents.openDevTools();
+
+  // Listen for window being closed
+  mainWindow.on('closed',  () => {
+    mainWindow = null
+  })
+}
+
+// Electron `app` is ready
+app.on('ready', createWindow)
+
+// Quit when all windows are closed - (Not macOS - Darwin)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
+app.on('activate', () => {
+  if (mainWindow === null) createWindow()
+})
